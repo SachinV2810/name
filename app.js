c3f8c3d90c372be03ab50e73b1436836aa73bbc4
SachinV2810 <134006040+SachinV2810@users.noreply.github.com>
2023-07-14 21:10:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let loginForm = document.getElementById("myform");
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let password = document.getElementById("inputPassword6");
+
+  if ( password.value == "sachin@1234") {
+    location.href="index.html";
+  } else {
+    alert("wrong password");
+    location.reload();
+;
+    password.value = "sachin@1234";
+  }
+});
